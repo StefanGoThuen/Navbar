7a34edfa9628807347282f5e9ce553a3ff4b45b3
stefthue58@gmail.com <stefthue58@gmail.com>
2023-10-31 17:29:13 +0100

    oppdatert filer

--- src/pages/blogs.js
+++ src/pages/blogs.js
@@ -3,7 +3,6 @@ import React from "react";
 const Blogs = () => {
   return (
     <div className="Navknapp">
-      <h1>Welcome to GeeksforGeeks Blogs</h1>
     </div>
   );
 };
--- src/pages/events.js
+++ src/pages/events.js
@@ -3,7 +3,6 @@ import React from "react";
 const Events = () => {
   return (
     <div className="Navknapp">
-      <h1>Welcome to GeeksforGeeks Events</h1>
     </div>
   );
 };
