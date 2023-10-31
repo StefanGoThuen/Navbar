1de971be58bab7d1f9306fe2919707e963285152
stefthue58@gmail.com <stefthue58@gmail.com>
2023-10-31 17:18:39 +0100

    Endringer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages";
+import About from "./pages/about";
+import Events from "./pages/events";
+import AnnualReport from "./pages/annual";
+import Teams from "./pages/team";
+import Blogs from "./pages/blogs";
+import SignUp from "./pages/signup";
+import SignIn from "./pages/signin";
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/annual" element={<AnnualReport />} />
+        <Route path="/team" element={<Teams />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+
+      </Routes>
+      </>
   );
 }
 
